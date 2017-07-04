463cf9a9b5ed6f395f198f9108ee5b5399baf3f1
mastercake10 <mczocker@gmx.net>
2017-07-04 21:35:59 +0200

    added content from old built jar, added src folder

---
+++ src/de/Linus122/customoregen/GeneratorConfig.java
@@ -0,0 +1,11 @@
+package de.Linus122.customoregen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GeneratorConfig {
+	public List<GeneratorItem> itemList = new ArrayList<GeneratorItem>();
+	public String permission = ";";
+	public int unlock_islandLevel = 0;
+}
