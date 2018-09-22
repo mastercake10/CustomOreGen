aafaf2d4252d56fb328c18d488caaca7093bd1ff
linus <dev@spaceio.de>
2018-09-22 05:05:23 +0200

    updated version

--- src/xyz/spaceio/customoregen/GeneratorConfig.java
+++ src/xyz/spaceio/customoregen/GeneratorConfig.java
@@ -3,9 +3,8 @@ package xyz.spaceio.customoregen;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GeneratorConfig {
-	public List<GeneratorItem> itemList = new ArrayList<GeneratorItem>();
-	public String permission = ";";
-	public int unlock_islandLevel = 0;
+    public List<GeneratorItem> itemList = new ArrayList<GeneratorItem>();
+    public String permission = ";";
+    public int unlock_islandLevel = 0;
 }
